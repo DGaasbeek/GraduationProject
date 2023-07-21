9c82d5d2572ea1ce1b67802c68dccbc6d376d8c5
DGaasbeek <94963819+DGaasbeek@users.noreply.github.com>
2023-07-21 16:03:34 +0200

    Add files via upload

---
+++ datasets/BPIC12W/prefix_extraction.sql
@@ -0,0 +1,24 @@
+SELECT caseid, task, end_timestamp, end_timestamp as time_copy, user as role
+        FROM original_df;
+
+SELECT a.caseid, min(a.end_timestamp) start_time
+        FROM raw a
+        GROUP BY a.caseid;
+
+SELECT a.*, b.start_time, ROW_NUMBER() OVER(
+            PARTITION BY a.caseid
+            ORDER BY end_timestamp asc) task_index
+        FROM raw a
+        JOIN cases b ON a.caseid = b.caseid;
+
+SELECT DISTINCT o.caseid, o.task_index AS milestone_index, o.task AS milestone, o2.task AS next_activity,
+        ROW_NUMBER() OVER(
+            PARTITION BY o.caseid
+            ORDER BY o.task_index) milestone_id
+        FROM traces o JOIN
+        traces o2 ON o.task_index = o2.task_index-1 AND o.caseid = o2.caseid;
+
+SELECT CAST(o.caseid AS TEXT)|| '_' ||CAST(m.milestone_id AS TEXT) AS prefix_id, o.caseid, o.task, 'role_'||CAST(o.role AS TEXT) as role, o.end_timestamp, o.start_time AS trace_start, ROUND((JULIANDAY(o.end_timestamp) - JULIANDAY(o.start_time))) AS timelapsed, ROUND((JULIANDAY(o.next_time) - JULIANDAY(o.start_time))) AS next_time, m.next_activity, m.milestone, m.milestone_id, o.task_index
+        FROM traces o
+        JOIN milestones m ON o.caseid = m.caseid AND o.task_index <= m.milestone_index;
+
